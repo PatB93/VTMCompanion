c9bffb3414c9c6eaad08adb538e6dd8c351827e6
PatB93 <pat.b.irish@gmail.com>
2020-07-14 10:03:57 -0400

    WIP

--- app/src/main/java/com/nokey/vtmcompanion/data/Clans.kt
+++ app/src/main/java/com/nokey/vtmcompanion/data/Clans.kt
@@ -9,3 +9,51 @@ enum class Clans(@StringRes val stringRes: Int) {
     TOREADOR(R.string.toreador),
     BRUJA(R.string.bruja),
 }
+
+enum class Attributes(attributeType: AttributeType) {
+    STRENGTH(AttributeType.PHYSICAL),
+    DEXTERITY(AttributeType.PHYSICAL),
+    STAMINA(AttributeType.PHYSICAL),
+    CHARISMA(AttributeType.SOCIAL),
+    MANIPULATION(AttributeType.SOCIAL),
+    COMPOSURE(AttributeType.SOCIAL),
+    INTELLIGENCE(AttributeType.MENTAL),
+    WITS(AttributeType.MENTAL),
+    RESOLVE(AttributeType.MENTAL)
+}
+
+enum class AttributeType{
+    PHYSICAL,
+    SOCIAL,
+    MENTAL
+}
+
+enum class Skills {
+    ATHLETICS,
+    BRAWL,
+    CRAFT,
+    DRIVE,
+    FIREARMS,
+    LARCENY,
+    MELEE,
+    STEALTH,
+    SURVIVAL,
+    ANIMAL_KEN,
+    ETIQUETTE,
+    INSIGHT,
+    INTIMIDATION,
+    LEADERSHIP,
+    PERFORMANCE,
+    PERSUASION,
+    STREETWISE,
+    SUBTERFUGE,
+    ACADEMICS,
+    AWARENESS,
+    FINANCE,
+    INVESTIGATION,
+    MEDICINE,
+    OCCULT,
+    POLITICS,
+    SCIENCE,
+    TECHNOLOGY
+}
